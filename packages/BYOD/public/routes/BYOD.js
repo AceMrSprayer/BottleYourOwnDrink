9f7a03fc6d267fe7033976a54496c56434450752
Lok Kwan Foo <lokkwanfoo@hotmail.com>
2015-05-21 13:41:36 +0200

    Added bottle1, bottle2. Registration and login modals are not fixed yet.

--- packages/BYOD/public/routes/BYOD.js
+++ packages/BYOD/public/routes/BYOD.js
@@ -2,9 +2,20 @@
 
 angular.module('mean.BYOD').config(['$meanStateProvider',
   function($meanStateProvider) {
+
     $meanStateProvider.state('BYOD example page', {
       url: '/BYOD',
       templateUrl: 'BYOD/views/index.html'
     });
+
+      $meanStateProvider.state('BYOD bottle page 1', {
+          url: '/bottle1',
+          templateUrl: 'BYOD/views/bottle1.html'
+      });
+
+      $meanStateProvider.state('BYOD bottle page 2', {
+          url: '/bottle2',
+          templateUrl: 'BYOD/views/bottle2.html'
+      });
   }
 ]);
