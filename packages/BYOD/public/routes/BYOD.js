caf4beb05d9337890f369cfd854e0c90bfbd1747
Sebastian Hilge <sebastianhilge@web.de>
2015-05-31 21:58:45 +0200

    Added confirmation pages and links

--- packages/BYOD/public/routes/BYOD.js
+++ packages/BYOD/public/routes/BYOD.js
@@ -21,8 +21,35 @@ angular.module('mean.BYOD').config(['$meanStateProvider',
 angular.module('mean.BYOD').config(['$meanStateProvider',
     function($meanStateProvider) {
         $meanStateProvider.state('Payment confirmation page Paypal', {
-            url: '/payment/Paypal',
-            templateUrl: 'BYOD/views/payment.html'
+            url: '/payment/:userID/confirmation-paypal',
+            templateUrl: 'BYOD/views/confirmationPaypal.html'
+        });
+    }
+]);
+
+angular.module('mean.BYOD').config(['$meanStateProvider',
+    function($meanStateProvider) {
+        $meanStateProvider.state('Payment confirmation page Ideal', {
+            url: '/payment/:userID/confirmation-ideal',
+            templateUrl: 'BYOD/views/confirmationIdeal.html'
+        });
+    }
+]);
+
+angular.module('mean.BYOD').config(['$meanStateProvider',
+    function($meanStateProvider) {
+        $meanStateProvider.state('Payment confirmation page Coupon', {
+            url: '/payment/:userID/confirmation-coupon',
+            templateUrl: 'BYOD/views/confirmationCoupon.html'
+        });
+    }
+]);
+
+angular.module('mean.BYOD').config(['$meanStateProvider',
+    function($meanStateProvider) {
+        $meanStateProvider.state('Payment confirmation page Credit', {
+            url: '/payment/:userID/confirmation-credit',
+            templateUrl: 'BYOD/views/confirmationCredit.html'
         });
     }
 ]);
